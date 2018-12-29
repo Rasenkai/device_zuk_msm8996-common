334e4bf6daf918d05e840d5fbf07ee97d5927546
aswin_a_s <aswinas7469@gmail.com>
2018-12-29 04:23:16 +0000

    Revert "msm8996-common: power: remove VR hints handling"
    This reverts commit 7b171110e99730cf9b4f0d5e7cf249269a65b26b.

--- power-libperfmgr/Power.h
+++ power-libperfmgr/Power.h
@@ -68,6 +68,7 @@ struct Power : public IPower {
 
     std::shared_ptr<HintManager> mHintManager;
     InteractionHandler mInteractionHandler;
+    std::atomic<bool> mVRModeOn;
     std::atomic<bool> mSustainedPerfModeOn;
     std::atomic<bool> mEncoderModeOn;
 };
